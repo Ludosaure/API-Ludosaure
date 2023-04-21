d8983abccb4ce6542edfdb34327028f38355cbe6
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2023-04-21 16:41:43 +0200

    Update update-reservation.handler.ts

--- src/modules/reservation/application/command/update-reservation.handler.ts
+++ src/modules/reservation/application/command/update-reservation.handler.ts
@@ -2,30 +2,45 @@ import {CommandHandler} from "@nestjs/cqrs";
 import {UpdateReservationCommand} from "./update-reservation.command";
 import {ReservationNotFoundException} from "../../exceptions/reservation-not-found.exception";
 import {ReservationEntityRepository} from "../../reservation-entity.repository";
+import {
+    EndDateBiggerThanStartDateException
+} from "../../../../shared/exceptions/end-date-bigger-than-start-date.exception";
+import {PlanEntityRepository} from "../../../plan/plan-entity.repository";
 
 @CommandHandler(UpdateReservationCommand)
 export class UpdateReservationHandler {
-    constructor(private readonly repository: ReservationEntityRepository) {
+    constructor(private readonly repository: ReservationEntityRepository,
+                private readonly planRepository: PlanEntityRepository) {
     }
 
     async execute(command: UpdateReservationCommand): Promise<void> {
         const foundReservation = await this.repository.findById(command.id);
-        if(foundReservation == null) {
+        if (foundReservation == null) {
             throw new ReservationNotFoundException();
         }
-        if(command.startDate != null) {
-            foundReservation.startDate = command.startDate;
+        if (command.startDate != null || command.endDate != null) {
+            if (command.startDate != null) {
+                foundReservation.startDate = command.startDate;
+            }
+            if (command.endDate != null) {
+                foundReservation.endDate = command.endDate;
+            }
+            if (foundReservation.startDate > foundReservation.endDate) {
+                throw new EndDateBiggerThanStartDateException();
+            }
+            foundReservation.appliedPlan = await this.planRepository.findByDuration(foundReservation.startDate, foundReservation.endDate);
+            foundReservation.totalAmount = foundReservation.calculateTotalAmount();
         }
-        if(command.endDate != null) {
-            foundReservation.endDate = command.endDate;
-        }
-        // TODO calculer nouveau prix
-        if(command.isReturned != null) {
+        if (command.isReturned != null) {
             foundReservation.isReturned = command.isReturned;
         }
-        if(command.isCancelled != null) {
+        if (command.isCancelled != null) {
             foundReservation.isCancelled = command.isCancelled;
         }
         await this.repository.saveOrUpdate(foundReservation);
+
+        // TODO - Générer une nouvelle facture pour la même réservation.
+        //  Il faut factuer la différence entre le montant total de la réservation et le montant déjà facturé.
+        // TODO - Envoyer un email à l'utilisateur
     }
 }
