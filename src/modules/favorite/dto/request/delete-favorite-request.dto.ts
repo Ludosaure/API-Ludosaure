33039637207b1c6d2e25137909f5630ad31bebbe
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2023-05-05 16:51:48 +0200

    [DEFAULT]

--- src/modules/favorite/dto/request/delete-favorite-request.dto.ts
+++ src/modules/favorite/dto/request/delete-favorite-request.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
 export class DeleteFavoriteRequestDto {
-  @ApiProperty({default: "b4d8b0a0-5e5a-4e1a-9b1a-8b0a0b4d5e5a"})
+  @ApiProperty({default: "2cfc2f43-5f32-405e-9cb2-26ac6e7ba615"})
   @IsNotEmpty()
   gameId: string;
 }
