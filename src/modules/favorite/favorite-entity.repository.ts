04a60af1dadf472fcbc511b9dd1848af8ac2a9d8
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2023-05-05 15:49:13 +0200

    [FAVORITE REPOSITORY]

---
+++ src/modules/favorite/favorite-entity.repository.ts
@@ -0,0 +1,46 @@
+import { Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { FavoriteGame } from "../../domain/model/favorite-game.entity";
+import { FavoriteRepository } from "../../infrastructure/favorite.repository";
+import { InjectRepository } from "@nestjs/typeorm";
+
+@Injectable()
+export class FavoriteEntityRepository extends Repository<FavoriteGame> implements FavoriteRepository {
+  constructor(@InjectRepository(FavoriteGame)
+              private favoriteRepository: Repository<FavoriteGame>) {
+    super(favoriteRepository.target,
+      favoriteRepository.manager,
+      favoriteRepository.queryRunner);
+  }
+
+  async findAll(): Promise<FavoriteGame[]> {
+    return await this.find();
+  }
+
+  async findByUserId(userId: string): Promise<FavoriteGame[]> {
+    return await this.find({
+      where: {
+        userId: userId
+      }
+    });
+  }
+
+  async findByGameId(gameId: string): Promise<FavoriteGame[]> {
+    return await this.find({
+      where: {
+        gameId: gameId
+      }
+    });
+  }
+
+  async saveOrUpdate(favoriteGame: FavoriteGame): Promise<void> {
+    await this.save(favoriteGame);
+  }
+
+  async deleteFavoriteGameByUserIdAndGameId(userId: string, gameId: string): Promise<void> {
+    await this.delete({
+      userId: userId,
+      gameId: gameId
+    });
+  }
+}
--- src/modules/favorite/favorite.module.ts
+++ src/modules/favorite/favorite.module.ts
@@ -4,11 +4,15 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {FavoriteGame} from "../../domain/model/favorite-game.entity";
 import {Game} from "../../domain/model/game.entity";
 import {User} from "../../domain/model/user.entity";
+import { FavoriteEntityRepository } from "./favorite-entity.repository";
 
 @Module({
     imports: [
         CqrsModule,
         TypeOrmModule.forFeature([FavoriteGame, Game, User]),
+    ],
+    providers: [
+      FavoriteEntityRepository,
     ],
 })
 export class FavoriteModule {
