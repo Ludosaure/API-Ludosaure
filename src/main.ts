ab8bc89561e137a5365afad2a8667729f1e2dfd2
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2023-05-12 10:20:38 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { config } from "dotenv";
+config();
 import { ClassSerializerInterceptor, INestApplication, Logger, ValidationPipe } from "@nestjs/common";
 import { NestFactory, Reflector } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { environmentConfig } from "./config/environment.config";
 import { config as awsConfig } from "aws-sdk";
-
-config();
 
 async function bootstrap() {
   const logger = new Logger("bootstrap");
