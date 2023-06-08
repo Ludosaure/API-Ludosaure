a90de9e38b4a73faf9787f2818ec520396a6e7d0
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2023-06-08 12:44:46 +0200

    Update generate-invoice.handler.ts

--- src/modules/invoice/application/command/generate-invoice.handler.ts
+++ src/modules/invoice/application/command/generate-invoice.handler.ts
@@ -12,26 +12,38 @@ const PDFTable = require("voilab-pdf-table");
 
 @CommandHandler(GenerateInvoiceCommand)
 export class GenerateInvoiceHandler implements ICommandHandler<GenerateInvoiceCommand> {
+  doc: any;
   constructor(
     private readonly invoiceRepository: InvoiceEntityRepository,
     private readonly invoiceService: InvoiceService
   ) {
+    this.doc = new PDFDocument();
   }
 
   async execute(command: GenerateInvoiceCommand): Promise<any> {
     const invoice = await this.invoiceRepository.findById(command.id);
 
-    const doc = new PDFDocument();
-
     const logo = await this.fetchImage(AppUtils.logoUrl);
     const filename = `facture_${invoice.invoiceNumber}.pdf`;
 
-    const totalTable = this.initTotalTable(invoice);
+    const totalHTByGame = invoice.invoiceGames.map(invoiceGame => {
+      const priceHT = invoiceGame.weeklyAmount * (1 - AppUtils.tva);
+      return priceHT * invoice.invoiceNbWeeks;
+    });
+
+    const totalHT = AppUtils.roundToTwoDecimals(totalHTByGame.reduce((globalTotalHT, gameTotalHT) => globalTotalHT + gameTotalHT, 0));
+    const totalTva = invoice.invoiceGames.map(invoiceGame => {
+      return invoiceGame.weeklyAmount * invoice.invoiceNbWeeks * AppUtils.tva;
+    }).reduce((globalTotalTva, gameTotalTva) => globalTotalTva + gameTotalTva, 0);
+    const totalReduction = AppUtils.roundToTwoDecimals((totalHT + totalTva) * (invoice.reduction / 100));
+
+    const totalTable = this.initTotalTable(totalHT, totalTva, totalReduction, invoice.amount);
 
     const alreadyPaidAmount = await this.invoiceService.getPreviouslyInvoicedAmountForReservation(invoice.reservation.id, invoice);
     const alreadyPaidWeeks = await this.invoiceService.getPreviouslyInvoicedWeeksForReservation(invoice.reservation.id, invoice);
+    
     // header
-    doc.image(logo, 50, 45, { width: 50 })
+    this.doc.image(logo, 50, 45, { width: 50 })
       .fillColor("#444444")
       .fontSize(20)
       .text(`La ludosaure - Facture #${invoice.invoiceNumber}`, 110, 57)
@@ -43,7 +55,7 @@ export class GenerateInvoiceHandler implements ICommandHandler<GenerateInvoiceCo
       .text(invoice.phone, { align: "right" });
 
     // body
-    doc.fontSize(10)
+    this.doc.fontSize(10)
       .text(`Réservation #${invoice.reservationNumber}`, 100)
       .text(`Facture créée le: ${invoice.createdAt.toLocaleDateString()}`)
       .moveDown()
@@ -54,29 +66,28 @@ export class GenerateInvoiceHandler implements ICommandHandler<GenerateInvoiceCo
       .moveDown()
       .text(`Nombre de semaines totales facturées: ${invoice.reservationNbWeeks}`);
     if (alreadyPaidAmount > 0 && alreadyPaidWeeks > 0) {
-      doc.text(`Montant déjà facturé: ${alreadyPaidAmount}€`)
+      this.doc.text(`Montant déjà facturé: ${alreadyPaidAmount}€`)
         .text(`Nombre de semaines déjà facturées: ${alreadyPaidWeeks}`);
     }
-    doc.moveDown(2);
+    this.doc.moveDown(2);
 
-    const invoiceTable = new PDFTable(doc);
+    const invoiceTable = new PDFTable(this.doc);
     this.initInvoiceTable(invoice, invoiceTable);
 
-    doc.moveDown(2)
+    this.doc.moveDown(2)
       .table(totalTable, {
         x: 170,
-        prepareHeader: () => doc.font("Helvetica-Bold").fontSize(10),
-        prepareRow: () => doc.font("Helvetica").fontSize(10)
+        prepareHeader: () => this.doc.font("Helvetica-Bold").fontSize(10),
+        prepareRow: () => this.doc.font("Helvetica").fontSize(10)
       });
 
     // footer
-    const totalReduction = AppUtils.roundToTwoDecimals(invoice.reservationTotalAmount * (invoice.reduction / 100) + Number.EPSILON);
-    doc.fontSize(8)
+    this.doc.fontSize(8)
       .text(`* La réduction est appliquée sur le prix total de la réservation. Le prix est soustrait au pro rata de ce qui a déjà été facturé`, 100)
       .text(`** Total réduction correspond à la réduction sur le montant de cette facture uniquement. La réduction totale au pro rata sur les précédentes factures est de ${totalReduction}€`)
       .end();
 
-    return new GenerateInvoiceResponseDto(doc, filename);
+    return new GenerateInvoiceResponseDto(this.doc, filename);
   }
 
   private initInvoiceTable(invoice: Invoice, invoiceTable: any) {
@@ -158,18 +169,7 @@ export class GenerateInvoiceHandler implements ICommandHandler<GenerateInvoiceCo
       }));
   }
 
-  private initTotalTable(invoice: Invoice) {
-    const totalHTByGame = invoice.invoiceGames.map(invoiceGame => {
-      const priceHT = invoiceGame.weeklyAmount * (1 - AppUtils.tva);
-      return priceHT * invoice.invoiceNbWeeks;
-    });
-
-    const totalHT = AppUtils.roundToTwoDecimals(totalHTByGame.reduce((globalTotalHT, gameTotalHT) => globalTotalHT + gameTotalHT, 0));
-    const totalTva = invoice.invoiceGames.map(invoiceGame => {
-      return invoiceGame.weeklyAmount * invoice.invoiceNbWeeks * AppUtils.tva;
-    }).reduce((globalTotalTva, gameTotalTva) => globalTotalTva + gameTotalTva, 0);
-    const reduction = AppUtils.roundToTwoDecimals((totalHT + totalTva) * (invoice.reduction / 100));
-
+  private initTotalTable(totalHT: number, totalTva: number, reduction: number, amount: number) {
     return {
       headers: [
         { label: "Total HT", property: "totalHT", width: 100, renderer: null },
@@ -182,7 +182,7 @@ export class GenerateInvoiceHandler implements ICommandHandler<GenerateInvoiceCo
           totalHT: totalHT + "€",
           totalTva: totalTva + "€",
           reduction: reduction + "€",
-          totalTTC: invoice.amount + "€"
+          totalTTC: amount + "€"
         }
       ]
     };
