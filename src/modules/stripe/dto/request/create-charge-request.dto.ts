5f73d192d903e74a79fd92db4b4e297fb12e737f
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2023-06-11 09:57:25 +0200

    [STRIPE]

--- src/modules/stripe/dto/request/create-charge-request.dto.ts
+++ src/modules/stripe/dto/request/create-charge-request.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString, IsNotEmpty, IsNumber } from "class-validator";
 
 export class CreateChargeRequestDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({default: "pm_card_visa"})
   paymentMethodId: string;
 
   @IsNumber()
-  amount: number;
+  @ApiProperty({default: 2500})
+  amount: number; // en centimes
 }
--- src/modules/stripe/stripe.controller.ts
+++ src/modules/stripe/stripe.controller.ts
@@ -1,17 +1,22 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
-import StripeService from '../stripe/stripe.service';
+import { Body, Controller, Post, Req, UseGuards } from "@nestjs/common";
+import StripeService from "../stripe/stripe.service";
 import { JwtAuthGuard } from "../../shared/guards/jwt-auth.guard";
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { CreateChargeRequestDto } from "./dto/request/create-charge-request.dto";
+import { RolesGuard } from "../../shared/guards/roles.guard";
+import { Roles } from "../../shared/roles.decorator";
+import { Role } from "../../domain/model/enum/role";
 
 @ApiTags('Stripe')
 @Controller('stripe')
-@UseGuards(JwtAuthGuard)
-export default class ChargeController {
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard, RolesGuard)
+export default class StripeController {
   constructor(
     private readonly stripeService: StripeService
   ) {}
 
+  @Roles(Role.CLIENT, Role.ADMIN)
   @Post('/charge')
   async createCharge(@Body() charge: CreateChargeRequestDto, @Req() request) {
     await this.stripeService.charge(charge.amount, charge.paymentMethodId, request.user.stripeCustomerId);
