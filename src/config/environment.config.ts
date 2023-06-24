7773165acc2e102472eae1c8d8c81328149fe883
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2023-06-24 14:52:41 +0200

    [CI/CD]

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -4,10 +4,6 @@ export interface IEnvironmentConfig {
     port: number;
     dbUrl: string;
     dataSourceType: any;
-    dataSourceHost: string;
-    dataSourceUsername: string;
-    dataSourcePassword: string;
-    dataSourcePort: number;
     cgvFilePath: string;
     awsRegion: string;
     awsAccessKeyId: string;
@@ -19,10 +15,6 @@ export class EnvironmentConfig implements IEnvironmentConfig {
     port: number;
     dbUrl: string;
     dataSourceType: any;
-    dataSourceHost: string;
-    dataSourceUsername: string;
-    dataSourcePassword: string;
-    dataSourcePort: number;
     cgvFilePath: string;
     awsRegion: string;
     awsAccessKeyId: string;
@@ -33,10 +25,6 @@ export class EnvironmentConfig implements IEnvironmentConfig {
         this.port = Number(process.env.PORT) || 3000;
         this.dbUrl = process.env.DATABASE_URL;
         this.dataSourceType = 'postgres';
-        this.dataSourceHost = process.env.RDS_HOSTNAME;
-        this.dataSourceUsername = process.env.RDS_USERNAME;
-        this.dataSourcePassword = process.env.RDS_PASSWORD;
-        this.dataSourcePort = Number(process.env.RDS_PORT);
         this.cgvFilePath = process.env.CGV_FILE_PATH;
         this.awsRegion = process.env.AWS_REGION;
         this.awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID;
