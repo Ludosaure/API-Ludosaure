06dd7152291f4e6a64969e6141a231cb9ee7d374
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2023-07-02 19:04:41 +0200

    [UPDATE PLAN]

--- src/modules/plan/application/command/update-plan.command.ts
+++ src/modules/plan/application/command/update-plan.command.ts
@@ -3,18 +3,16 @@ import {UpdatePlanRequestDto} from "../../dto/request/update-plan-request.dto";
 export class UpdatePlanCommand {
     public readonly id: string;
     public readonly name: string;
-    public readonly reduction: number;
-    public readonly nbWeeks: number;
+    public readonly isActive: boolean;
 
-    private constructor(id: string, name: string, reduction: number, nbWeeks: number) {
+    private constructor(id: string, name: string, isActive: boolean) {
         this.id = id;
         this.name = name;
-        this.reduction = reduction;
-        this.nbWeeks = nbWeeks;
+        this.isActive = isActive;
     }
 
     public static of(updatePlanRequestDto: UpdatePlanRequestDto): UpdatePlanCommand {
-        const {id, name, reduction, nbWeeks} = updatePlanRequestDto;
-        return new UpdatePlanCommand(id, name, reduction, nbWeeks);
+        const {id, name, isActive} = updatePlanRequestDto;
+        return new UpdatePlanCommand(id, name, isActive);
     }
 }
